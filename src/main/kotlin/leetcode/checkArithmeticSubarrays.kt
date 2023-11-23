0317e2a6965001ffad1fa6a52f7aac23eaf3f4ce
Isma3ilMohamed <dev.ismail.mohamed@gmail.com>
2023-11-23 23:30:50 +0200

    1630. Arithmetic Subarrays

---
+++ src/main/kotlin/leetcode/checkArithmeticSubarrays.kt
@@ -0,0 +1,27 @@
+package leetcode
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+fun check(arr: IntArray): Boolean? {
+    Arrays.sort(arr)
+    val diff = arr[1] - arr[0]
+    for (i in 2..<arr.size) {
+        if (arr[i] - arr[i - 1] != diff) {
+            return false
+        }
+    }
+    return true
+}
+
+fun checkArithmeticSubarrays(nums: IntArray, l: IntArray, r: IntArray): List<Boolean?>? {
+    val ans: MutableList<Boolean?> = ArrayList()
+    for (i in l.indices) {
+        val arr = IntArray(r[i] - l[i] + 1)
+        for (j in arr.indices) {
+            arr[j] = nums[l[i] + j]
+        }
+        ans.add(check(arr))
+    }
+    return ans
+}
