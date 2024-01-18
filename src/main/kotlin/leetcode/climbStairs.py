171339b9c0146659cc7f14aae9db2775cd438874
Isma3ilMohamed <dev.ismail.mohamed@gmail.com>
2024-01-18 15:55:02 +0200

    70. Climbing Stairs

---
+++ src/main/kotlin/leetcode/climbStairs.py
@@ -0,0 +1,9 @@
+def climbStairs(n: int) -> int:
+    prev_prev = 1
+    prev = 2
+    for i in range(2, n + 1):
+        prev, prev_prev = prev + prev_prev, prev
+    return prev_prev
+
+
+print(climbStairs(6))
