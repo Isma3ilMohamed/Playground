1d7b4820963ff6f192dc6823655e27ac3799c280
Isma3ilMohamed <dev.ismail.mohamed@gmail.com>
2024-01-19 14:23:45 +0200

    931. Minimum Falling Path Sum [DP]

--- src/main/kotlin/leetcode/minFallingPathSum.py
+++ src/main/kotlin/leetcode/minFallingPathSum.py
@@ -1,28 +1,17 @@
 class Solution:
     def minFallingPathSum(self, matrix: list[list[int]]) -> int:
         N = len(matrix)
-        cache = {}
 
-        def dfs(r, c):
-            if r == N:
-                return 0
-            if c < 0 or c == N:
-                return float("inf")
-            if (r, c) in cache:
-                return cache[(r, c)]
-            res = matrix[r][c] + min(
-                dfs(r + 1, c - 1),
-                dfs(r + 1, c),
-                dfs(r + 1, c + 1),
-            )
-            cache[(r, c)] = res
-            return res
+        for r in range(1, N):
+            for c in range(N):
+                mid = matrix[r - 1][c]
+                left = matrix[r - 1][c - 1] if c > 0 else float("inf")
+                right = matrix[r - 1][c + 1] if c < N-1 else float("inf")
+                matrix[r][c] = matrix[r][c]+min(mid,left,right)
 
-        res = float("inf")
-        for c in range(N):
-            res = min(res, dfs(0, c))
-        return res
-
+        return min(matrix[-1])
+# First solution is recursive with memoization
+# Second solution is DP
 
 solution = Solution()
 print(solution.minFallingPathSum([[2, 1, 3], [6, 5, 4], [7, 8, 9]]))
