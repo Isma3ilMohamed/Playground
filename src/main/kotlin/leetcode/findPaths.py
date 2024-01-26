021662ebdbbc2791c65ed6d28d341c03815dc846
Isma3ilMohamed <dev.ismail.mohamed@gmail.com>
2024-01-26 16:16:34 +0200

    576. Out of Boundary Paths DP (Bottom Up) solution

--- src/main/kotlin/leetcode/findPaths.py
+++ src/main/kotlin/leetcode/findPaths.py
@@ -2,26 +2,30 @@ class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         ROWS, COLS = m, n
         MOD = 10 ** 9 + 7
-        cache = {}
+        grid = [[0] * COLS for r in range(ROWS)]
 
-        def dfs(r, c, moves):
-            if (r < 0 or r == ROWS or
-                    c < 0 or c == COLS):
-                return 1
-            if moves == 0:
-                return 0
-            if (r, c, moves) in cache:
-                return cache[(r, c, moves)]
-
-            cache[(r, c, moves)] = (
-                                           (dfs(r + 1, c, moves - 1) +
-                                            dfs(r - 1, c, moves - 1)) % MOD +
-                                           (dfs(r, c + 1, moves - 1) +
-                                            dfs(r, c - 1, moves - 1)) % MOD
-                                   ) % MOD
-            return cache[(r, c, moves)]
-
-        return dfs(startRow, startColumn, maxMove)
+        for m in range(1, maxMove + 1):
+            tmp = [[0] * COLS for r in range(ROWS)]
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if r + 1 == ROWS:
+                        tmp[r][c] = (tmp[r][c] + 1) % MOD
+                    else:
+                        tmp[r][c] = (tmp[r][c] + grid[r + 1][c]) % MOD
+                    if r - 1 < 0:
+                        tmp[r][c] = (tmp[r][c] + 1) % MOD
+                    else:
+                        tmp[r][c] = (tmp[r][c] + grid[r - 1][c]) % MOD
+                    if c + 1 == COLS:
+                        tmp[r][c] = (tmp[r][c] + 1) % MOD
+                    else:
+                        tmp[r][c] = (tmp[r][c] + grid[r][c + 1]) % MOD
+                    if c - 1 < 0:
+                        tmp[r][c] = (tmp[r][c] + 1) % MOD
+                    else:
+                        tmp[r][c] = (tmp[r][c] + grid[r][c - 1]) % MOD
+            grid = tmp
+        return grid[startRow][startColumn]
 
 
 solution = Solution()
