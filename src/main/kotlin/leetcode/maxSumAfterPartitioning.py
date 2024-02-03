1fa20a2eeab99dc46ed1b7ca99bddabdef515239
Isma3ilMohamed <dev.ismail.mohamed@gmail.com>
2024-02-03 17:31:16 +0200

    1043. Partition Array for Maximum Sum
    DP

--- src/main/kotlin/leetcode/maxSumAfterPartitioning.py
+++ src/main/kotlin/leetcode/maxSumAfterPartitioning.py
@@ -1,22 +1,23 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: list[int], k: int) -> int:
+        dp = [0] * k
+        dp[0] = arr[0]
 
-        cache={}
-        def dfs(i):
-            if i in cache:
-                return cache[i]
+        for i in range(1, len(arr)):
+            cur_max = 0
+            max_at_i = 0
+            for j in range(i,i-k,-1):
+                if j< 0:
+                    break
+                cur_max=max(cur_max,arr[j])
+                window_size=i-j+1
+                cur_sum=cur_max * window_size
+                sub_sum=dp[(j-1)%k] if j> 0 else dp[-1]
+                max_at_i=max(max_at_i,cur_sum+sub_sum)
+            dp[i%k] = max_at_i
 
-            curr_max=0
-            res = 0
-            for j in range(i,min(len(arr),i+k)):
-                curr_max=max(curr_max,arr[j])
-                window_size= j -i +1
-                res=max(res,dfs(j+1) + curr_max * window_size)
-            cache[i] = res
-            return res
-
-        return dfs(0)
+        return dp[(len(arr)-1) % k]
 
 
-solution=Solution()
-print(solution.maxSumAfterPartitioning(arr=[1,15,7,9,2,5,10],k=3))
+solution = Solution()
+print(solution.maxSumAfterPartitioning(arr=[1, 15, 7, 9, 2, 5, 10], k=3))
