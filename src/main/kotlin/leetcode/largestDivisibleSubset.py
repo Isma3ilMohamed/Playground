7b755271e2b889901584ba688cc6ceddb76d9cd4
Ismail Mohamed <dev.ismail.mohamed@gmail.com>
2024-02-09 20:48:05 +0200

    DP

--- src/main/kotlin/leetcode/largestDivisibleSubset.py
+++ src/main/kotlin/leetcode/largestDivisibleSubset.py
@@ -1,25 +1,12 @@
 class Solution:
-    def largestDivisibleSubset(self, nums: list[int]) -> list[int]:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
-        cache ={}
-        def dfs(i):
-            if i == len(nums):  return []
-
-            if i in cache:  return cache[i]
-
-            res = [nums[i]]
-            for j in range(i + 1, len(nums)):
-                if nums[j] % nums[i] == 0:
-                    tmp = [nums[i]] + dfs(j)
-
-                    if len(tmp) > len(res):
-                        res = tmp
-            cache[i] = res
-            return res
-
+        n = len(nums)
+        dp = [[val] for val in nums]
         res = []
-        for i in range(len(nums)):
-            tmp = dfs(i)
-            if len(tmp) > len(res):
-                res = tmp
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                if nums[j] % nums[i] == 0 and len(dp[j]) >= len(dp[i]):
+                    dp[i] = [nums[i]] + dp[j]
+            res = dp[i] if len(dp[i]) > len(res) else res
         return res
