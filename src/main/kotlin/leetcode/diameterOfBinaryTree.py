4ef626dfabefc59ecefffae0bce89cc144ddfb17
Ismail Mohamed <dev.ismail.mohamed@gmail.com>
2024-02-27 11:20:10 +0200

    543. Diameter of Binary Tree

---
+++ src/main/kotlin/leetcode/diameterOfBinaryTree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, res):
+            if not node:
+                return 0
+            left = dfs(node.left, res)
+            right = dfs(node.right, res)
+
+            res[0] = max(res[0], left + right)
+            return max(left, right) + 1
+
+        res = [0]
+        dfs(root, res)
+        return res[0]
