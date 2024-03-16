ec686bede26f45ae0d7c3c6ee1eedc5f5fc10a6e
Ismail Mohamed <dev.ismail.mohamed@gmail.com>
2024-03-16 02:48:20 +0200

    525. Contiguous Array

---
+++ src/main/kotlin/leetcode/findMaxLength.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        totalsum,hashmap=0,{0:-1}
+        res,diff=0,0
+        for i in range(len(nums)):
+            if(nums[i]==0):
+                totalsum-=1
+            else:
+                totalsum+=1
+            try:
+                diff=i-hashmap[totalsum]
+                if(diff>res):
+                    res=diff
+            except:
+                hashmap[totalsum]=i
+        return res
+        
