811661c58d9c21a4ab2a7800ba7e06422af7a01b
Ismail Mohamed <dev.ismail.mohamed@gmail.com>
2024-03-24 12:58:28 +0200

    287. Find the Duplicate Number Attempted
    List solution
    
    Signed-off-by: Ismail Mohamed <dev.ismail.mohamed@gmail.com>

--- src/main/kotlin/leetcode/findDuplicate.py
+++ src/main/kotlin/leetcode/findDuplicate.py
@@ -1,8 +1,9 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-
-        for key,item in counter.items():
-            if item > 1:
-                return key
+        counts = set()
+        for val in nums:
+            if val in counts:
+                return val
+            else:
+                counts.add(val)
         return 0
