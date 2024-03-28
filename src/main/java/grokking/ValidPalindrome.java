e377d4b05b31ff32dd5526677084bcd89692c4f6
Isma3ilMohamed <dev.ismail.mohamed@gmail.com>
2024-03-28 09:33:54 +0200

    Two Pointers Tech Valid Palindrome

---
+++ src/main/java/grokking/ValidPalindrome.java
@@ -0,0 +1,25 @@
+package grokking;
+
+public class ValidPalindrome {
+
+    public static boolean isPalindrome(String s){
+        int left=0;
+        int right =s.length()-1;
+
+        while (left<=right){
+            if (s.charAt(left) != s.charAt(right)){
+                return false;
+            }else{
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abba"));
+    }
+}
