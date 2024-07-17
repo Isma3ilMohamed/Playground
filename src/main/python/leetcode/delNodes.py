921cedc55724492900676484e61862f859de58a0
Ismail Mohamed <dev.ismail.mohamed@gmail.com>
2024-07-17 15:05:58 +0300

    1110. Delete Nodes And Return Forest
    Signed-off-by: Ismail Mohamed <dev.ismail.mohamed@gmail.com>

---
+++ src/main/python/leetcode/delNodes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
+        to_delete_set = set(to_delete)
+        result = []
+        
+        def dfs(node: Optional[TreeNode], is_root: bool) -> Optional[TreeNode]:
+            if not node:
+                return None
+            
+            root_deleted = node.val in to_delete_set
+            if is_root and not root_deleted:
+                result.append(node)
+            
+            node.left = dfs(node.left, root_deleted)
+            node.right = dfs(node.right, root_deleted)
+            
+            return None if root_deleted else node
+        
+        dfs(root, True)
+        return result
