b718cd7c18f9344dfb3e683042db1b95620a43bb
Ismail Mohamed <dev.ismail.mohamed@gmail.com>
2025-05-30 03:43:25 +0300

    2359. Find Closest Node to Given Two Nodes
    Signed-off-by: Ismail Mohamed <dev.ismail.mohamed@gmail.com>

---
+++ src/main/python/leetcode/closestMeetingNode.py
@@ -0,0 +1,33 @@
+class Solution:
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        visited1 = set()
+        visited2 = set()
+        while node1 != -1 or node2 != -1:
+            if node1 in visited1:
+                node1 = -1
+
+            if node2 in visited2:
+                node2 = -1
+
+            if node1 != -1:
+                visited1.add(node1)
+
+            if node2 != -1:
+                visited2.add(node2)
+
+            if node1 in visited2 and node2 in visited1:
+                return min(node1, node2)
+
+            if node1 in visited2:
+                return node1
+
+            if node2 in visited1:
+                return node2
+
+            if node1 != -1:
+                node1 = edges[node1]
+
+            if node2 != -1:
+                node2 = edges[node2]
+
+        return -1
